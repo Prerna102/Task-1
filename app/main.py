f07c22f0924f05089739791e2e84d944e5ca99c7
Prerna102 <prernapandey@onfinity.io>
2026-09-17 12:41:38 +0530

    paddleocr

--- app/main.py
+++ app/main.py
@@ -1,25 +1,52 @@
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 from app.db.database import Base, engine
-
 from app.api.routes import health
 from app.api.routes import ocr
 
+from app.workers.ocr_worker import ocr_worker
+import asyncio
 
-# --------------------------------------------------
+
 # Database
-# --------------------------------------------------
-
 # Create database tables
 Base.metadata.create_all(
     bind=engine
 )
 
 
-# --------------------------------------------------
-# FastAPI
-# --------------------------------------------------
+#Application lifespan
+@asynccontextmanager
+async def lifespan(app: FastAPI):
 
+    # Start 2 background OCR workers
+    worker_tasks = [
+        asyncio.create_task(ocr_worker()),
+        asyncio.create_task(ocr_worker()),
+    ]
+
+    print("OCR workers started: 2")
+
+    try:
+        # Keep FastAPI running
+        yield
+
+    finally:
+        # Stop workers when FastAPI shuts down
+        print("Stopping OCR workers...")
+
+        for task in worker_tasks:
+            task.cancel()
+
+        await asyncio.gather(
+            *worker_tasks,
+            return_exceptions=True,
+        )
+
+        print("OCR workers stopped.")
+
+
+# FastAPI
 app = FastAPI(
     title="PaddleOCR API",
     description=(
@@ -27,13 +54,11 @@ app = FastAPI(
         "PaddleOCR and PostgreSQL"
     ),
     version="1.0.0",
+    lifespan=lifespan,
 )
 
 
-# --------------------------------------------------
 # Routers
-# --------------------------------------------------
-
 app.include_router(
     health.router,
     prefix="/health",
@@ -47,10 +72,7 @@ app.include_router(
 )
 
 
-# --------------------------------------------------
 # Root
-# --------------------------------------------------
-
 @app.get("/")
 def root():
     return {
