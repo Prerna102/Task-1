67e6daf48aad3e37b6e7cf9a2eb2252e53b6768c
Prerna102 <prernapandey@onfinity.io>
2026-09-21 15:53:48 +0530

    .

--- app/workers/ocr_worker.py
+++ app/workers/ocr_worker.py
@@ -11,7 +11,7 @@ ocr_queue = asyncio.Queue()
 
 
 # Maximum number of OCR operations allowed simultaneously.
-ocr_semaphore = asyncio.Semaphore(2)
+ocr_semaphore = asyncio.Semaphore(4)
 
 
 def save_processing_result(
